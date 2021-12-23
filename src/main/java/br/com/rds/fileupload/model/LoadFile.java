d582815a175511f4a9b7263351f9a48590c28566
Rogério Domingos <rogerio.ds@hotmail.com>
2021-12-23 16:06:02 -0300

    Initial commit

---
+++ src/main/java/br/com/rds/fileupload/model/LoadFile.java
@@ -0,0 +1,43 @@
+package br.com.rds.fileupload.model;
+
+public class LoadFile {
+
+    private String fileName;
+    private String fileType;
+    private String fileSize;
+    private byte[] file;
+
+    public LoadFile() {}
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+}
